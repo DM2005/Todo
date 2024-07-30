c0144493fe61e38664b4056798d5c04a8ea9443d
Devansh <mdevansh0109@gmail.com>
2024-07-30 11:51:14 +0530

    todo

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def login(request):
     return render(request,"accounts/login.html")
 
+def register(request):
+    return render(request, "accounts/register.html")
 # Create your views here.
